5981aae22797e416962cf29c0821b8a91c770677
Onam Kumar Verma <106578262+onamkrverma@users.noreply.github.com>
2024-08-16 09:56:12 +0530

    Update page.tsx

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -61,8 +61,8 @@ export default function Home() {
         },
         {
           enableHighAccuracy: false,
-          maximumAge: 10 * 1000,
-          timeout: 10 * 1000,
+          maximumAge: 60 * 1000,
+          timeout: 30 * 1000,
         }
       );
       return () => {
